fed5d38b1eb86e1798428e080d565a07e9640410
polygonator <32782446+polygonator@users.noreply.github.com>
2017-12-23 19:20:51 +0300

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -6,7 +6,7 @@
 		{
 			name: 'Facebook',
 			class: 'facebook',
-			url: 'https://www.facebook.com/sharer.php?s=100&p[url]={url}&p[images][0]={img}&p[title]={title}&p[summary]={desc}'
+			url: 'http://www.facebook.com/sharer.php?s=100&p[url]={url}&p[images][0]={img}&p[title]={title}&p[summary]={desc}'
 		},
 		{
 			name: 'Facebook (share dialog)',
