d0c41cb67ad990196c0bd4807837f0055f19aa8c
Denis Pantykin <dpsw@yandex.ru>
2020-02-02 15:18:15 +0300

    Footer clients slider

--- js/main.js
+++ js/main.js
@@ -17,10 +17,48 @@ $(document).ready(function() {
         window.location = $(this).val();
     });
 
-    
+
     $('#contact').on('submit', function(e) {
         $('#contact *').fadeOut(2000);
         $('#submitted_message').prepend('Your submission has been processed...');
+    });
+
+    $(".clients-footer").slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: false,
+        centerMode: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
     });
 
 });
